5f323dabf921e0b1c7b5a05d428f6d3a59c9ad0a
cynthiachinenye <cynthiachiagu@gmail.com>
2023-08-26 01:08:32 +0100

    new

--- myapp/src/Components/Footer.js
+++ myapp/src/Components/Footer.js
@@ -1,5 +1,10 @@
 import React from 'react'
-import Insta from '@mui/material'
+import Insta from  '@mui/icons-material/Instagram'
+import FaceBook from '@mui/icons-material/Facebook'
+import Twitter from '@mui/icons-material/Twitter'
+import LinkedIn from '@mui/icons-material/LinkedIn'
+import '../ForStyles/Footer.css'
+
 
 function Footer() {
   return (
@@ -7,8 +12,11 @@ function Footer() {
       
       <div className="socialMedia">
        <Insta/>
+       <FaceBook/>
+       <Twitter/>
+       <LinkedIn/>
       </div>
-      
+      <p>&copy; 2023 cyndymengpractice.com</p>      
     </div>
   )
 }
