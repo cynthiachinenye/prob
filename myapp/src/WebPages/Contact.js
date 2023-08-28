9305079ce97a776e6f5f126c3003332333b0fa43
cynthiachinenye <cynthiachiagu@gmail.com>
2023-08-28 03:05:51 +0100

    new

--- myapp/src/WebPages/Contact.js
+++ myapp/src/WebPages/Contact.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import contactImage from '../Assets/pexels-photo-280453.jpeg'
+import '../ForStyles/Contact.css'
 
 function Contact() {
   return (
     <div className="contact">
-    <div style={{backgroundImage: `url($(contactImage))`}}></div>
+    <div style={{backgroundImage: `url(${contactImage})`}}></div>
+     <div className='contact-content'>
      <h1> Contact Us</h1>
      <form id='name' method='POST'>
       <label>Full Name</label>
@@ -13,9 +15,12 @@ function Contact() {
       <input type='email' placeholder='Enter Email' name='email'/>
       <label>Message</label>
       <textarea placeholder='Enter Your Message' type='text'></textarea>
-     
+       
+      <button type='submit'>Send Message</button>
+
      </form>
-      
+           
+     </div>
     </div>
   )
 }
